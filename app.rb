ae9550de336b4d47911184f3e3ef6865ca45fff9
Luciano Oliveira and Megan Olson <megan@>
2017-09-12 16:13:41 -0700

    Add full funcitonality and css

--- app.rb
+++ app.rb
@@ -14,3 +14,19 @@ end
 get('/maze2') do
  erb(:maze2)
 end
+
+get('/maze3') do
+  erb(:maze3)
+end
+
+get('/maze4') do
+ erb(:maze4)
+end
+
+get('/maze5') do
+ erb(:maze5)
+end
+
+get('/maze6') do
+ erb(:maze6)
+end
--- spec/maze_spec.rb
+++ spec/maze_spec.rb
@@ -1,11 +1,17 @@
 #!/usr/bin/env ruby
-require 'rspec'
-require 'maze'
-
-describe('Maze#result') do
-  it "will allow user to go to the next phase" do
-    maze = Maze.new("begin")
-    expect(maze.result).to(eq(true))
-  end
-  # it "will go to first maze page when first button is clicked"
-end
+# require 'rspec'
+# require 'maze'
+#
+# describe('Maze#opening') do
+#   it "will allow user to go to the next phase" do
+#     maze = Maze.new("begin")
+#     expect(maze.opening).to(eq(true))
+#   end
+# end
+# #
+# # describe('Maze#maze1') do
+# #   it "will go to first maze page when first button is clicked" do
+# #     maze = Maze.new("window")
+# #     expect(maze.maze1).to(eq(true))
+# #   end
+# # end
